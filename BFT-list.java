997e3e633605608c48c04ff4aa60ac8858247774
SajidAhmad41 <sajidahmadsk2005@gmail.com>
2025-02-24 21:26:18 +0530

    Create BFT-list.java

---
+++ BFT-list.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph {
+    private LinkedList<Integer>[] adjacencyList;
+    private int numVertices;
+
+    public Graph(int numVertices) {
+        this.numVertices = numVertices;
+        adjacencyList = new LinkedList[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int i, int j) {
+        adjacencyList[i].add(j);
+        adjacencyList[j].add(i); // For undirected graph
+    }
+
+    public void BFT(int startVertex) {
+        boolean[] visited = new boolean[numVertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            for (int neighbor : adjacencyList[vertex]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+}
