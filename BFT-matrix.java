bc5ae2d3c70898bef50f938385eb16fdbece1689
SajidAhmad41 <sajidahmadsk2005@gmail.com>
2025-02-24 21:25:51 +0530

    Create BFT-matrix.java

---
+++ BFT-matrix.java
@@ -0,0 +1,41 @@
+// Adjacency matrix
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Graph {
+    private int[][] adjacencyMatrix;
+    private int numVertices;
+
+    public Graph(int numVertices) {
+        this.numVertices = numVertices;
+        adjacencyMatrix = new int[numVertices][numVertices];
+    }
+
+    public void addEdge(int i, int j) {
+        adjacencyMatrix[i][j] = 1;
+        adjacencyMatrix[j][i] = 1; // For undirected graph
+    }
+
+    public void BFT(int startVertex) {
+        boolean[] visited = new boolean[numVertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            for (int i = 0; i < numVertices; i++) {
+                if (adjacencyMatrix[vertex][i] == 1 && !visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                }
+            }
+        }
+    }
+}
+
+
+
